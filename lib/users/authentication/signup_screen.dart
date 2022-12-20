7252e78b527990a10332b84c7388d6f1d029fe27
ngynkhnhlnh <nguyenkhanhlinh2752001@gmail.com>
2022-12-20 19:33:51 +0700

    feat/register

--- lib/users/authentication/signup_screen.dart
+++ lib/users/authentication/signup_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
 import 'dart:html';
 
 import 'package:books_app/users/authentication/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
 
 class SignUpScreen extends StatefulWidget {
 
@@ -14,14 +18,66 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen>
 {
   var formKey = GlobalKey<FormState>();
-  var nameController = TextEditingController();
+  var fullNameController = TextEditingController();
+  var usernameController = TextEditingController();
   var emailController = TextEditingController();
+  var addressController = TextEditingController();
+  var phoneNumberController = TextEditingController();
   var passwordController = TextEditingController();
   var isObsecure = true.obs;
+
+  registerAndSaveUserRecord() async
+  {
+    try
+    {
+      var res = await http.post(
+          Uri.parse("https://localhost:7075/gateway/register"),
+          body: jsonEncode({
+              "fullName": fullNameController.text.trim(),
+              "username": usernameController.text.trim(),
+              "email": emailController.text.trim(),
+              "address": addressController.text.trim(),
+              "phoneNumber": phoneNumberController.text.trim(),
+              "password": passwordController.text.trim()
+          }),
+          headers: {
+            "Accept": "application/json",
+            "content-type":"application/json"
+          }
+
+      );
+
+      if(res.statusCode == 200) //from flutter app the connection with api to server - success
+          {
+        var resBodyOfSignUp = jsonDecode(res.body);
+        if(resBodyOfSignUp['isSuccess'] == true)
+        {
+          Fluttertoast.showToast(msg: "Congratulations, you are SignUp Successfully.");
+          setState(() {
+            emailController.clear();
+            passwordController.clear();
+            emailController.clear();
+            passwordController.clear();
+            emailController.clear();
+            passwordController.clear();
+          });
+        }
+        else
+        {
+          Fluttertoast.showToast(msg: "Error Occurred, Try Again.");
+        }
+      }
+    }
+    catch(e)
+    {
+      print(e.toString());
+      Fluttertoast.showToast(msg: e.toString());
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:  Colors.black,
+      backgroundColor:  Colors.deepPurpleAccent,
       body: LayoutBuilder(
         builder: (context,cons){
           return ConstrainedBox(
@@ -71,16 +127,16 @@ class _SignUpScreenState extends State<SignUpScreen>
                               child: Column(
                                 children: [
 
-                                  //name
+                                  //fullName
                                   TextFormField(
-                                    controller: nameController,
-                                    validator: (val) => val == ""? "Please write name" : null,
+                                    controller: fullNameController,
+                                    validator: (val) => val == ""? "Please write fullName" : null,
                                     decoration: InputDecoration(
                                       prefixIcon: const Icon(
                                         Icons.person,
                                         color: Colors.black,
                                       ),
-                                      hintText: "name...",
+                                      hintText: "fullName...",
                                       border: OutlineInputBorder(
                                         borderRadius: BorderRadius.circular(30),
                                         borderSide: const BorderSide(
@@ -118,9 +174,55 @@ class _SignUpScreenState extends State<SignUpScreen>
                                       filled: true,
                                     ),
                                   ),
-
                                   const SizedBox(height: 18,),
+                                  //username
+                                  TextFormField(
+                                    controller: usernameController,
+                                    validator: (val) => val == ""? "Please write username" : null,
+                                    decoration: InputDecoration(
+                                      prefixIcon: const Icon(
+                                        Icons.person,
+                                        color: Colors.black,
+                                      ),
+                                      hintText: "username...",
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                          color: Colors.white60,
+                                        ),
 
+                                      ),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                          color: Colors.white60,
+                                        ),
+
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                          color: Colors.white60,
+                                        ),
+
+                                      ),
+                                      disabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                          color: Colors.white60,
+                                        ),
+
+                                      ),
+                                      contentPadding: const EdgeInsets.symmetric(
+                                        horizontal: 14,
+                                        vertical: 6,
+
+                                      ),
+                                      fillColor: Colors.white,
+                                      filled: true,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 18,),
                                   //email
                                   TextFormField(
                                     controller: emailController,
@@ -168,8 +270,106 @@ class _SignUpScreenState extends State<SignUpScreen>
                                       filled: true,
                                     ),
                                   ),
+
                                   const SizedBox(height: 18,),
 
+                                  //address
+                                  TextFormField(
+                                    controller: addressController,
+                                    validator: (val) => val == ""? "Please write address" : null,
+                                    decoration: InputDecoration(
+                                      prefixIcon: const Icon(
+                                        Icons.abc_outlined,
+                                        color: Colors.black,
+                                      ),
+                                      hintText: "address...",
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                          color: Colors.white60,
+                                        ),
+
+                                      ),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                          color: Colors.white60,
+                                        ),
+
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                          color: Colors.white60,
+                                        ),
+
+                                      ),
+                                      disabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                          color: Colors.white60,
+                                        ),
+
+                                      ),
+                                      contentPadding: const EdgeInsets.symmetric(
+                                        horizontal: 14,
+                                        vertical: 6,
+
+                                      ),
+                                      fillColor: Colors.white,
+                                      filled: true,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 18,),
+
+                                  //phoneNumber
+                                  TextFormField(
+                                    controller: phoneNumberController,
+                                    validator: (val) => val == ""? "Please write phoneNumber" : null,
+                                    decoration: InputDecoration(
+                                      prefixIcon: const Icon(
+                                        Icons.phone,
+                                        color: Colors.black,
+                                      ),
+                                      hintText: "phoneNumber...",
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                          color: Colors.white60,
+                                        ),
+
+                                      ),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                          color: Colors.white60,
+                                        ),
+
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                          color: Colors.white60,
+                                        ),
+
+                                      ),
+                                      disabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: const BorderSide(
+                                          color: Colors.white60,
+                                        ),
+
+                                      ),
+                                      contentPadding: const EdgeInsets.symmetric(
+                                        horizontal: 14,
+                                        vertical: 6,
+
+                                      ),
+                                      fillColor: Colors.white,
+                                      filled: true,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 18,),
                                   //password
                                   Obx(() =>TextFormField(
                                     controller: passwordController,
@@ -242,7 +442,10 @@ class _SignUpScreenState extends State<SignUpScreen>
                                     child: InkWell(
                                       onTap: ()
                                       {
-
+                                        if(formKey.currentState!.validate())
+                                        {
+                                          registerAndSaveUserRecord();
+                                        }
                                       },
                                       borderRadius: BorderRadius.circular(30),
                                       child: const Padding(
